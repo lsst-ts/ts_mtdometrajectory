c5eeb5019995a5b2736e819e7f95378b276e8765
Russell Owen <rowen@uw.edu>
2020-06-13 09:58:34 -0700

    Implement MTMockDome

--- python/lsst/ts/MTDomeTrajectory/__init__.py
+++ python/lsst/ts/MTDomeTrajectory/__init__.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from .mock_dome import *
 try:
     from .version import *
 except ImportError:
